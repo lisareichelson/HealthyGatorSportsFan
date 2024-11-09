7a101edb90b694134f1e4210cf88bcc1c5d600f8
Shannon <shanhix1@Outlook.com>
2024-11-09 11:24:51 -0800

    Trying to get cfbd api connected, saving progress

--- HealthyGatorSportsFanDjango/project/settings.py
+++ HealthyGatorSportsFanDjango/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--7brl&&mqp0y=9%ae82(02f)74p8hl7+d^1obwvrgu0=&b^(k*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.68.124', '192.168.4.32']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.68.124', '192.168.4.32', 'b862-184-185-222-16.ngrok-free.app']
 
 
 # Application definition
