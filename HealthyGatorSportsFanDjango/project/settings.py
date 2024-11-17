ba07c38c2b8309cad5e368d785243506e76069d9
amy-e-stone <aestone04@gmail.com>
2024-11-17 08:55:15 -0500

    Correct app path

--- HealthyGatorSportsFanDjango/project/settings.py
+++ HealthyGatorSportsFanDjango/project/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'HealthyGatorSportsFanDjango.app',
     'rest_framework',
     'corsheaders',
 ]
