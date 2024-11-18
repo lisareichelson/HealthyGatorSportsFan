55a23fde0f327819f1e46353df1dcb62c1357ba5
amy-e-stone <aestone04@gmail.com>
2024-11-18 15:05:46 -0500

    Add whitenoise so Heroku properly serves static files (to fix admin page)

--- HealthyGatorSportsFanDjango/project/settings.py
+++ HealthyGatorSportsFanDjango/project/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,9 @@ STATIC_URL = 'static/'
 # Heroku requires this for serving static files in production.
 # All static assets are collected into one location before deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # HealthyGatorSportsFanDjango/staticfiles
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
